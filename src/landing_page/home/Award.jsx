7d654f32a0ee8179375ca3d091d87258d4fded81
Antish Kumar Yadav <antishyadav726@gmail.com>
2026-02-01 00:56:06 +0530

    award component created

--- src/landing_page/home/Award.jsx
+++ src/landing_page/home/Award.jsx
@@ -1,7 +1,34 @@
 function Award(){
     return(
-        <div>
-            <h1>Award Section</h1>
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-6 p-5">
+                    <img src="/media/images/largestBroker.svg"></img>
+                </div>
+                <div className="col-6 p-5">
+                    <h2>Largest stock broker in India</h2>
+                    <p>2+ million Trader client contribute to over 15% of all retail order 
+                        volume in India by trading and investing in:
+                    </p>
+                    <div className="row mt-5">
+                        <div className="col-6">
+                            <ul>
+                                <li><p>Future and Options</p></li>
+                                <li><p>Commodity derivatives</p></li>
+                                <li><p>Currency derivatives</p></li>
+                            </ul>
+                        </div>
+                        <div className="col-6">
+                            <ul>
+                                <li><p>Stoks & IPOs</p></li>
+                                <li><p>Direct mutual funds</p></li>
+                                <li><p>Bonds and Govt. securities</p></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <img src="/media/images/pressLogos.png" style={{width:"90%"}}></img>
+                </div>
+            </div>
         </div>
     )
 }
--- src/landing_page/home/Stats.jsx
+++ src/landing_page/home/Stats.jsx
@@ -2,8 +2,42 @@ import React from 'react';
 
 function Stats(){
     return (
-        <div>
-            <h1>Stats </h1>
+        <div className='container p-3'>
+            <div className='row p-5'>
+            <div className='col-6 p-5'>
+                <h1 className='fs-2 mb-5'>Trust with confidence</h1>
+                
+                <h2 class='fs-4'>Customer-first always</h2>
+                <p class='text-muted'>That's why 1.3+ crore customers trust Zerodha with 3.5+
+                    lakh crores worth of equity investments.
+                </p>
+        
+                <h2 class='fs-4'>No spam or gimmicks</h2>
+                <p class='text-muted'>No gimmicks, spam, "gamification, or annoying push notification. 
+                    High quality apps that you use at your pace the way you like
+                </p>
+            
+            
+                <h2 class='fs-4'>The Zerodha universe</h2>
+                <p class='text-muted'>Not just an app, but a whole ecosystem. Our investement in 
+                    30+ fintech startups offer tailored services specific to you needs.
+                </p>
+            
+                <h2 class='fs-4'>Do better with money</h2>
+                <p class='text-muted'>With initiatives like Nudge and Kill Switch, we don't just 
+                    facilitate transaction, but actively help you do better with 
+                    you money
+                </p>
+                
+            </div>
+            <div className='col-6 p-5 mt-5'>
+                <img src="/media/images/ecosystem.png" style={{width:"95%"}} className='mx-auto'/>
+                <div class='text-center'>
+                <a href="" class='mx-5' style={{textDecoration:"none"}}>Explore a products <i class="fa-solid fa-arrow-right"></i></a>
+                <a href="" style={{textDecoration:"none"}}>Try kite</a>
+            </div>
+                </div>
+            </div>
         </div>
     )
 }
